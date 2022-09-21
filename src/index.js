fa095352948db7a14edad389f6c8dec2ff79d6d7
Rychart Redwerkz <redwerkz@users.noreply.github.com>
2022-09-21 09:27:58 +0200

    🎨 remove root id

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 import './index.css'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementsByTagName("main")[0])
 root.render(
   <React.StrictMode>
     <App />
